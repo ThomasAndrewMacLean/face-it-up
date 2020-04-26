4776574a5ec4805782b2da137daadf4f74e4a5d3
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-04-26 09:30:43 +0200

    ios bug

--- utils/index.js
+++ utils/index.js
@@ -94,6 +94,47 @@ export const picFile = (setLoading, sethasPic) => {
         canvas.height = smallest;
         canvas.style.height = smallest;
 
+        const iOS =
+              !!navigator.platform &&
+              /iPad|iPhone|iPod/.test(navigator.platform);
+            // alert(orientation);
+
+            if (iOS) {
+              switch (orientation) {
+                case 2:
+                  ctx.translate(width, 0);
+                  ctx.scale(-1, 1);
+                  break;
+                case 3:
+                  ctx.translate(width, height);
+                  ctx.rotate(Math.PI);
+                  break;
+                case 4:
+                  ctx.translate(0, height);
+                  ctx.scale(1, -1);
+                  break;
+                case 5:
+                  ctx.rotate(0.5 * Math.PI);
+                  ctx.scale(1, -1);
+                  break;
+                case 6:
+                  ctx.rotate(0.5 * Math.PI);
+                  ctx.translate(0, -height);
+                  break;
+                case 7:
+                  ctx.rotate(0.5 * Math.PI);
+                  ctx.translate(width, -height);
+                  ctx.scale(-1, 1);
+                  break;
+                case 8:
+                  ctx.rotate(-0.5 * Math.PI);
+                  ctx.translate(-width, 0);
+                  break;
+                default:
+                  break;
+              }
+            }
+
         ctx.drawImage(
           img,
           0,
